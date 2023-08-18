397e8a854b7ec4fd63b2c7a4d0cde65fbf1d99f6
Burin-Zhargal <papaev.b.n@gmail.com>
2023-08-18 20:14:04 +0300

    add firebase remote config

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -9,37 +11,13 @@ import 'package:sportquiz/screens/home.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  await fetchRemoteConfig();
   final sharedPreferences = await SharedPreferences.getInstance();
   final savedUrl = sharedPreferences.getString('savedUrl');
   if (savedUrl != null) {
     // Open the saved URL immediately
     runApp(MyApp(initialUrl: savedUrl));
   } else {
-    await fetchRemoteConfig();
     runApp(MyApp());
-  }
-}
-
-Future<void> fetchRemoteConfig() async {
-  final FirebaseRemoteConfig remoteConfig = FirebaseRemoteConfig.instance;
-
-  // Add default values to Remote Config
-  remoteConfig.setDefaults(<String, dynamic>{
-    'url': 'https://google.com',
-  });
-
-  try {
-    // Fetch remote config values and activate them
-    await remoteConfig.fetchAndActivate();
-
-    // Save the URL locally
-    final sharedPreferences = await SharedPreferences.getInstance();
-    final url = remoteConfig.getString('url');
-    await sharedPreferences.setString('savedUrl', url);
-  } catch (e) {
-    // Handle any errors during fetch or activation
-    print('Error fetching remote config: $e');
   }
 }
 
@@ -55,7 +33,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomePage(),
+      home: MainView(initialUrl: initialUrl),
       debugShowCheckedModeBanner: false,
     );
   }
@@ -75,60 +53,141 @@ class _MainViewState extends State<MainView> {
   double progress = 0;
 
   Future<bool> onBackPressed() async {
-    _webViewController.goBack();
-    return false;
+    if (await _webViewController.canGoBack()) {
+      _webViewController.goBack();
+      return false;
+    } else {
+      // Block system back button on starting page
+      return true;
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        body: WillPopScope(
-          onWillPop: onBackPressed,
-          child: ListView(
-            scrollDirection: Axis.horizontal,
-            children: [
-              Container(
-                width: MediaQuery.of(context)
-                    .size
-                    .width, // Set width to fit the entire screen
-                child: Stack(
-                  children: [
-                    InAppWebView(
-                      initialUrlRequest: URLRequest(
-                          url: Uri.parse(
-                              widget.initialUrl ?? 'https://bjgh.github.io/')),
-                      initialOptions: InAppWebViewGroupOptions(
-                        crossPlatform: InAppWebViewOptions(
-                          useShouldOverrideUrlLoading: true,
-                          javaScriptCanOpenWindowsAutomatically: true,
+      child: FutureBuilder<SharedPreferences>(
+        future: SharedPreferences.getInstance(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final sharedPreferences = snapshot.data!;
+            final savedUrl = sharedPreferences.getString('savedUrl');
+            //final savedUrl = null;
+            //final deviceBrand = 'google'; // TODO: Get actual device brand
+
+            if (savedUrl == null) {
+              // Show splash screen if saved URL is empty
+              return HomePage();
+            } else {
+              // Show InAppWebView with URL
+              return Scaffold(
+                body: WillPopScope(
+                  onWillPop: onBackPressed,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context)
+                            .size
+                            .width, // Set width to fit the entire screen
+                        child: Stack(
+                          children: [
+                            InAppWebView(
+                              initialUrlRequest:
+                                  URLRequest(url: Uri.parse('savedUrl')),
+                              initialOptions: InAppWebViewGroupOptions(
+                                crossPlatform: InAppWebViewOptions(
+                                  useShouldOverrideUrlLoading: true,
+                                  javaScriptCanOpenWindowsAutomatically: true,
+                                  javaScriptEnabled: true,
+                                ),
+                              ),
+                              onWebViewCreated: (controller) {
+                                _webViewController = controller;
+                              },
+                              onProgressChanged: (controller, progress) {
+                                setState(() {
+                                  this.progress = progress / 100;
+                                });
+                              },
+                            ),
+                            progress < 1.0
+                                ? Center(
+                                    child: CircularProgressIndicator(
+                                      value: progress,
+                                      color: Color.fromARGB(255, 54, 244, 177),
+                                      strokeWidth: 2,
+                                    ),
+                                  )
+                                : Container(),
+                          ],
                         ),
                       ),
-                      onWebViewCreated: (controller) {
-                        _webViewController = controller;
-                      },
-                      onProgressChanged: (controller, progress) {
-                        setState(() {
-                          this.progress = progress / 100;
-                        });
-                      },
-                    ),
-                    progress < 1.0
-                        ? Center(
-                            child: CircularProgressIndicator(
-                              value: progress,
-                              color: Color.fromARGB(255, 54, 244, 177),
-                              strokeWidth: 2,
-                            ),
-                          )
-                        : Container(),
-                  ],
+                    ],
+                  ),
                 ),
+              );
+            }
+          } else {
+            return Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
               ),
-            ],
-          ),
-        ),
+            );
+          }
+        },
       ),
     );
+  }
+}
+
+class Application {
+  final String json;
+
+  Application({required this.json});
+
+  String? getUrl() {
+    final jsonMap = jsonDecode(json);
+    return jsonMap['url'];
+  }
+}
+
+Future<Application> receiveApplicationJson(String json) async {
+  // Process the JSON and save it to the device
+  final sharedPreferences = await SharedPreferences.getInstance();
+  await sharedPreferences.setString('savedJson', json);
+
+  return Application(json: json);
+}
+
+Future<Application> getSavedApplication() async {
+  final sharedPreferences = await SharedPreferences.getInstance();
+  final savedJson = sharedPreferences.getString('savedJson');
+  if (savedJson == null) {
+    throw Exception('No saved JSON found');
+  }
+  return Application(json: savedJson);
+}
+
+Future<Application> getRemoteApplication() async {
+  final FirebaseRemoteConfig remoteConfig = FirebaseRemoteConfig.instance;
+
+  // Add default values to Remote Config
+  remoteConfig.setDefaults(<String, dynamic>{
+    'json': '{}',
+  });
+
+  try {
+    // Fetch remote config values and activate them
+    await remoteConfig.fetchAndActivate();
+
+    // Parse the JSON to get the URL
+    final jsonStr = remoteConfig.getString('json');
+    final json = jsonDecode(jsonStr);
+
+    return Application(json: jsonStr);
+  } catch (e) {
+    // Handle any errors during fetch or activation
+    print('Error fetching remote config: $e');
+    throw Exception('Error fetching remote config');
   }
 }
